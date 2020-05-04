6536f95ba07268561e3b0905e0c9ea9850fb0b69
Timjoco <timjoco13@gmail.com>
2020-05-03 20:12:06 -0500

    removed dev keys from server side

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '392739489696-ro3ma3mr0footplddhkp4f8627q1vuvh.apps.googleusercontent.com',
-  googleClientSecret: 'bhWMHkA-9SwAEjlYggXaBEvr',
-  mongoURI:
-    'mongodb+srv://catUser1:KZL5YbPtuWWoziju@cluster0-br67i.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: 'hjkfdhjklafdnaiowq89h3n21rbf',
-};
